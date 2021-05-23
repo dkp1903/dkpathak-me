8c1cf486b9958aff93973f9013fddcaeb5281cb4
Dushyant Pathak <dushyantpathak6789@gmail.com>
2021-05-23 14:38:34 +0530

    basher commit

--- content/jobs/Sample/index.md
+++ content/jobs/Sample/index.md
@@ -1,6 +1,6 @@
 ---
 date: '2017-12-21'
-title: 'UI Engineer Co-op'
+title: 'abc Co-op'
 company: 'Apple'
 location: 'Cupertino, CA'
 range: 'July - December 2017'
--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'dushyantpathak6789@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/dkp1903',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/haq_se_dkp',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/dkp1903',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/dkp1903',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/dkp1903',
     },
   ],
 
