13a255375e2f436528d55ceebadd88ab5ab450c9
dkp1903 <dushyantpathak6789@gmail.com>
2021-06-06 09:38:52 +0000

    basher commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Dushyant Pathak',
+    description: 'DKP is a full stack engineer who has been building apps that scale for 2+ years',
+    siteUrl: 'https://dkp.today', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@dkp1903',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
