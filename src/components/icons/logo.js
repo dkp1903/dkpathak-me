a6e93fda3328bb781c94739da49ce6b53e1726fb
dkp1903 <dushyantpathak6789@gmail.com>
2021-06-10 05:55:27 +0000

    basher commit

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
+    <title>Log</title>
     <g transform="translate(-8.000000, -2.000000)">
       <g transform="translate(11.000000, 5.000000)">
-       <path d="M320 213.83L266.3 247.47L266.3 314.75L320 348.4L373.7 314.75L373.7 247.47L320 213.83Z" id="c14RuQYKpB" />
+       <path d="M320 213.83L266.3 247.47L266.3 314.75L320 348.4L373.7 314.75L373.7 247.47L320 213.83Z" fill="currentColor" id="c14RuQYKpB" />
         <polygon
           id="Shape"
           stroke="currentColor"
